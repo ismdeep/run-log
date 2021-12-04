c45570cf070f5302305e33ef37a513e03c8ad4de
L. Jiang <l.jiang.1024@gmail.com>
2021-12-04 17:50:51 +0800

    update

--- main.go
+++ main.go
@@ -5,11 +5,90 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"sync"
 	"time"
 )
 
+const OutputEnvName = "OUTPUT"
+const BreakLineEnvName = "BREAK_LINE"
+
+func showHelp() {
+	fmt.Println("Usage: [OUTPUT=<output log file path|default for stdout>] [BREAK_LINE=<true:default|false>] run-log <commands>")
+}
+
+func ParseOutPointer() *os.File {
+	out := os.Stdout
+	outStr := os.Getenv(OutputEnvName)
+	if outStr != "" {
+		var err error
+		out, err = os.OpenFile(outStr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return out
+}
+
+func ParseBreakLine() bool {
+	s := os.Getenv(BreakLineEnvName)
+	if s == "" {
+		return true
+	}
+	a, _ := strconv.ParseBool(s)
+	return a
+}
+
+func watchLine(out *os.File, r *bufio.Reader, outType string, w *sync.WaitGroup) {
+	outTypeStr := fmt.Sprintf("[%v]", outType)
+	line := ""
+	for {
+		tmp, isPrefix, err := r.ReadLine()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			_, _ = fmt.Fprintln(out, time.Now().Format(time.RFC3339), "[ERR]", err.Error())
+		}
+		line += string(tmp)
+		if isPrefix {
+			continue
+		}
+		_, _ = fmt.Fprintln(out, time.Now().Format(time.RFC3339), outTypeStr, line)
+		line = ""
+	}
+	w.Done()
+}
+
+func watchBreakLine(out *os.File, r *bufio.Reader, outType string, w *sync.WaitGroup) {
+	outTypeStr := fmt.Sprintf("[%v]", outType)
+	for {
+		line, _, err := r.ReadLine()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			_, _ = fmt.Fprintln(out, time.Now().Format(time.RFC3339), "[ERR]", err.Error())
+		}
+
+		_, _ = fmt.Fprintln(out, time.Now().Format(time.RFC3339), outTypeStr, string(line))
+	}
+	w.Done()
+}
+
 func main() {
+	if len(os.Args) <= 1 || os.Args[1] == "--help" {
+		showHelp()
+		return
+	}
+
+	out := ParseOutPointer()
+	defer func() {
+		_ = out.Close()
+	}()
+
+	breakLine := ParseBreakLine()
+
 	name := os.Args[1]
 	args := make([]string, 0)
 	for i := 2; i < len(os.Args); i++ {
@@ -32,26 +111,16 @@ func main() {
 		panic(err)
 	}
 
-	watchLine := func(r *bufio.Reader, outType string, w *sync.WaitGroup) {
-		outTypeStr := fmt.Sprintf("[%v]", outType)
-		for {
-			line, _, err := r.ReadLine()
-			if err != nil {
-				if err.Error() == "EOF" {
-					break
-				}
-				fmt.Println(time.Now().Format(time.RFC3339), "[ERR]", err.Error())
-			}
-			fmt.Println(time.Now().Format(time.RFC3339), outTypeStr, string(line))
-		}
-		w.Done()
-	}
-
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	wg.Add(1)
-	go watchLine(stdoutPipeReader, "INFO", wg)
-	go watchLine(stderrPipeReader, "ERR", wg)
+	if breakLine {
+		go watchBreakLine(out, stdoutPipeReader, "INFO", wg)
+		go watchBreakLine(out, stderrPipeReader, "ERR", wg)
+	} else {
+		go watchLine(out, stdoutPipeReader, "INFO", wg)
+		go watchLine(out, stderrPipeReader, "ERR", wg)
+	}
 
 	wg.Wait()
 }
